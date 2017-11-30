700ca301295d2fc15a0129cdc3005332d78e90cc
Shivam Krishan Sharma <33988926+stormshadow07@users.noreply.github.com>
2017-12-01 00:44:32 +0530

    initial commit

---
+++ upload.php
@@ -0,0 +1,13 @@
+<?PHP
+  if(!empty($_FILES['uploaded_file']))
+  {
+    $path = "upload/";
+    $path = $path . basename( $_FILES['uploaded_file']['name']);
+    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+      echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
+      " has been uploaded";
+    } else{
+        echo "There was an error uploading the file, please try again!";
+    }
+  }
+?>
